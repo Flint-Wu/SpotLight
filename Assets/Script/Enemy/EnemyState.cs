d073c6038ea156b290c7ebaef22f2294f89f872d
Flint-Wu <1196428456@qq.com>
2024-10-12 01:45:54 +0800

    初步创建敌人的通用数据

---
+++ Assets/Script/Enemy/EnemyState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour
+{
+    public string EnemyName;
+    public int damage;
+
+    private Collider2D coll;
+    private Rigidbody2D rig;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
